0c11212612916e23ba047ad7305722a0405a3637
Matthew Doles <matteddied@gmail.com>
2020-03-02 15:26:00 -0500

    Try/Catch logic for History operations

--- test/history.test.js
+++ test/history.test.js
@@ -1,4 +1,10 @@
-const { login, recent, recentAllObjects, recentlyUpdated, recentlyDeleted } = require('../index');
+const {
+  login,
+  recent,
+  recentAllObjects,
+  recentlyUpdated,
+  recentlyDeleted
+} = require('../index');
 
 test('Should retrieve recenelty accessed Account records (w/ callback)', async () => {
   const conn = await login({
@@ -22,6 +28,19 @@ test('Should retrieve recenelty accessed Account records (w/out callback)', asyn
   expect(res).toBeTruthy();
 });
 
+test('Should fail to retrieve recenelty accessed Account records', async () => {
+  const conn = await login({
+    username: process.env.SF_USERNAME,
+    password: process.env.SF_PASSWORD
+  });
+
+  const recentAccountRecords = await recent(conn, 'Accout', (err, res) => {
+    expect(err).toBeTruthy();
+    expect(res).toBeFalsy();
+  });
+  expect(recentAccountRecords).toBeTruthy();
+});
+
 test('Should retrieve all recenelty accessed records (w/ callback)', async () => {
   const conn = await login({
     username: process.env.SF_USERNAME,
@@ -34,7 +53,7 @@ test('Should retrieve all recenelty accessed records (w/ callback)', async () =>
   });
 });
 
-test('Should retrieve recenelty accessed Account records (w/out callback)', async () => {
+test('Should retrieve all recenelty accessed records (w/out callback)', async () => {
   const conn = await login({
     username: process.env.SF_USERNAME,
     password: process.env.SF_PASSWORD
@@ -42,6 +61,19 @@ test('Should retrieve recenelty accessed Account records (w/out callback)', asyn
 
   const res = await recentAllObjects(conn);
   expect(res).toBeTruthy();
+});
+
+test('Should fail to retrieve all recenelty accessed records', async () => {
+  const conn = await login({
+    username: 'invalidusername',
+    password: process.env.SF_PASSWORD
+  });
+
+  const recentRecords = await recentAllObjects(conn, (err, res) => {
+    expect(err).toBeTruthy();
+    expect(res).toBeFalsy();
+  });
+  expect(recentRecords).toBeTruthy();
 });
 
 test('Should retrieve all updated records within given time period (w/ callback)', async () => {
@@ -74,6 +106,24 @@ test('Should retrieve all updated records within given time period (w/out callba
   expect(res).toBeTruthy();
 });
 
+test('Should fail to retrieve all updated records within given time period', async () => {
+  const conn = await login({
+    username: 'invalidusername',
+    password: process.env.SF_PASSWORD
+  });
+
+  const updatedRecords = await recentlyUpdated(
+    conn,
+    'Contact',
+    '2020-02-27',
+    '2020-02-26',
+    (err, res) => {
+      expect(err).toBeTruthy();
+      expect(res).toBeFalsy();
+    }
+  );
+  expect(updatedRecords).toBeTruthy();
+});
 
 test('Should retrieve all deleted records within given time period (w/ callback)', async () => {
   const conn = await login({
@@ -103,4 +153,23 @@ test('Should retrieve all deleted records within given time period (w/out callba
     '2020-02-26'
   );
   expect(res).toBeTruthy();
+});
+
+test('Should fail to retrieve all deleted records within given time period', async () => {
+  const conn = await login({
+    username: 'invalidusername',
+    password: process.env.SF_PASSWORD
+  });
+
+  const deletedRecords = await recentlyDeleted(
+    conn,
+    'Contact',
+    '2020-02-27',
+    '2020-02-26',
+    (err, res) => {
+      expect(err).toBeTruthy();
+      expect(res).toBeFalsy();
+    }
+  );
+  expect(deletedRecords).toBeTruthy();
 });
